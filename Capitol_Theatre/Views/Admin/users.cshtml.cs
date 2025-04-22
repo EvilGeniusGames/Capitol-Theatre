b0c93be60757075546407063e6b7b79c083a78f3
richard_barnes <richard_barnes@gov.nt.ca>
2025-04-22 09:45:11 -0600

    Added User Management

---
+++ Capitol_Theatre/Views/Admin/users.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Capitol_Theatre.Views.Admin
+{
+    public class usersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
