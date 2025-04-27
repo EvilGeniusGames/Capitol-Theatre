7bf382dec5506337cf64b7e98e5582e98ae45224
Richard Barnes <richardcbarnes@gmail.com>
2025-04-27 09:59:57 -0600

    Implement sortable movie management interface.

---
+++ Capitol_Theatre/Models/MovieListing.cs
@@ -0,0 +1,10 @@
+﻿using Capitol_Theatre.Data;
+
+namespace Capitol_Theatre.Models
+{
+    public class MovieListing
+    {
+        public Movie Movie { get; set; } = null!;
+        public string Status { get; set; } = "";
+    }
+}
