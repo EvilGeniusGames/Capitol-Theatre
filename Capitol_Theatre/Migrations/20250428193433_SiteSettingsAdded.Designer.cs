fee527c2dfabb771ac7bdedf0619e360682d5c0a
Richard Barnes <richardcbarnes@gmail.com>
2025-04-28 15:38:33 -0600

    WIP: mid day commit

--- Capitol_Theatre/Migrations/20250428193433_SiteSettingsAdded.Designer.cs
+++ Capitol_Theatre/Migrations/20250428193433_SiteSettingsAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Capitol_Theatre.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Capitol_Theatre.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250428193433_SiteSettingsAdded")]
+    partial class SiteSettingsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
